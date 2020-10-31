602fbe66429ba58a0b206378cfcea7fdab6cabb3
F0Xde <foxcraftextreme@gmail.com>
2020-10-31 08:49:29 +0100

    Store translations with argument type

--- src/main/kotlin/Lokales.kt
+++ src/main/kotlin/Lokales.kt
@@ -2,13 +2,11 @@ package de.f0x.lokales
 
 import java.util.*
 import kotlin.collections.HashMap
-import kotlin.reflect.KType
-import kotlin.reflect.full.isSupertypeOf
-import kotlin.reflect.full.starProjectedType
+import kotlin.reflect.KClass
 import kotlin.reflect.full.valueParameters
 import kotlin.reflect.jvm.reflect
 
-class LokaleBundle internal constructor(
+class LokaleBundle(
     val locale: Locale,
     private val translations: Map<String, Translation>
 ) {
@@ -17,65 +15,82 @@ class LokaleBundle internal constructor(
 }
 
 class LokaleBundleBuilder(val locale: Locale) {
-    private val translations: MutableMap<String, Translation> = HashMap()
+    val translations: MutableMap<String, Translation> = HashMap()
 
     infix fun String.to(value: String) {
         translations[this] = ValTranslation(value)
     }
 
-    infix fun <A> String.to(fn: (String) -> String) {
+    inline infix fun <reified A> String.to(noinline fn: (String) -> String) {
         // Invoking the function manually has to be done because `KFunction.call` is not yet implemented
-        translations[this] = FnTranslation(fn.namedArgs) { fn(it[0]) }
+        translations[this] = FnTranslation(fn.withTypes(A::class)) { fn(it[0]) }
     }
 
-    infix fun <A, B> String.to(fn: (String, String) -> String) {
-        translations[this] = FnTranslation(fn.namedArgs) { fn(it[0], it[1]) }
+    inline infix fun <reified A, reified B> String.to(
+        noinline fn: (String, String) -> String
+    ) {
+        translations[this] = FnTranslation(fn.withTypes(A::class, B::class)) {
+            fn(it[0], it[1])
+        }
     }
 
-    infix fun <A, B, C> String.to(fn: (String, String, String) -> String) {
-        translations[this] = FnTranslation(fn.namedArgs) { fn(it[0], it[1], it[2]) }
+    inline infix fun <reified A, reified B, reified C> String.to(
+        noinline fn: (String, String, String) -> String
+    ) {
+        translations[this] = FnTranslation(fn.withTypes(A::class, B::class, C::class)) {
+            fn(it[0], it[1], it[2])
+        }
     }
 
-    infix fun <A, B, C, D> String.to(fn: (String, String, String, String) -> String) {
-        translations[this] = FnTranslation(fn.namedArgs) { fn(it[0], it[1], it[2], it[3]) }
+    inline infix fun <reified A, reified B, reified C, reified D> String.to(
+        noinline fn: (String, String, String, String) -> String
+    ) {
+        translations[this] = FnTranslation(
+            fn.withTypes(A::class, B::class, C::class, D::class)
+        ) {
+            fn(it[0], it[1], it[2], it[3])
+        }
     }
 
-    infix fun <A, B, C, D, E> String.to(fn: (String, String, String, String, String) -> String) {
-        translations[this] = FnTranslation(fn.namedArgs) { fn(it[0], it[1], it[2], it[3], it[4]) }
+    inline infix fun <reified A, reified B, reified C, reified D, reified E> String.to(
+        noinline fn: (String, String, String, String, String) -> String
+    ) {
+        translations[this] = FnTranslation(
+            fn.withTypes(A::class, B::class, C::class, D::class, E::class)
+        ) {
+            fn(it[0], it[1], it[2], it[3], it[4])
+        }
     }
 
     fun build() = LokaleBundle(locale, translations)
-
-    private val Function<*>.namedArgs: List<Pair<String, KType>>
-        get() = reflect()!!.valueParameters.map {
-            it.name!! to it.type
-        }
 }
 
 fun lokale(locale: Locale, init: LokaleBundleBuilder.() -> Unit) =
     LokaleBundleBuilder(locale).apply(init).build()
 
-internal interface Translation {
+interface Translation {
     operator fun get(vararg args: Any?): String
 }
 
-internal class ValTranslation(private val value: String) : Translation {
+class ValTranslation(private val value: String) : Translation {
     override fun get(vararg args: Any?) = value
 }
 
-internal class FnTranslation(
-    private val args: List<Pair<String, KType>>,
+class FnTranslation(
+    private val args: List<Pair<String, KClass<*>>>,
     private val fn: (List<String>) -> String
 ) : Translation {
     override fun get(vararg args: Any?): String {
+        lokale(Locale.US) {
+            "test" to "abc"
+            "test1".to<String> { name -> "Welcome, $name" }
+        }
         if (args.size != this.args.size) {
             throw IllegalArgumentException("Wrong number of arguments passed to Translation")
         }
         val stringArgs = args.zip(this.args).map { (arg, expected) ->
             val (name, type) = expected
-            if ((arg == null && type.isMarkedNullable) ||
-                (arg != null && type.isSupertypeOf(arg::class.starProjectedType))
-            ) {
+            if (type.isInstance(arg)) {
                 // TODO special conversion for numbers, dates, etc.
                 arg.toString()
             } else {
@@ -85,3 +100,8 @@ internal class FnTranslation(
         return fn(stringArgs)
     }
 }
+
+val Function<*>.argNames: List<String>
+    get() = reflect()!!.valueParameters.mapNotNull { it.name }
+
+fun Function<*>.withTypes(vararg types: KClass<*>) = argNames.zip(types)
