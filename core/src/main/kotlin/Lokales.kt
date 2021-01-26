e77378f834f547bbc7bfd9c8073b015c2a424a96
F0X <f0xde@protonmail.com>
2021-01-26 17:50:29 +0100

    Throw exception on missing key

--- core/src/main/kotlin/Lokales.kt
+++ core/src/main/kotlin/Lokales.kt
@@ -6,15 +6,18 @@ class TranslationBundle(
     val locale: Locale,
     private val translations: Map<String, Translation>
 ) {
-    operator fun get(key: String) =
-        translations[key]?.get(*arrayOfNulls(0))
+    operator fun get(key: String): String =
+        translations[key]?.get(*arrayOfNulls(0)) ?: throw MissingTranslationKey(key, locale)
 
-    operator fun get(key: String, vararg args: Any?) =
-        translations[key]?.get(*args)
+    operator fun get(key: String, vararg args: Any?): String =
+        translations[key]?.get(*args) ?: throw MissingTranslationKey(key, locale)
 
-    operator fun get(key: String, vararg args: Pair<String, Any?>) =
-        translations[key]?.get(*args)
+    operator fun get(key: String, vararg args: Pair<String, Any?>): String =
+        translations[key]?.get(*args) ?: throw MissingTranslationKey(key, locale)
 }
+
+class MissingTranslationKey(val key: String, val locale: Locale) :
+    Exception("Translation key '$key' not present in bundle for $locale")
 
 fun lokale(locale: Locale, init: TranslationBundleBuilder.() -> Unit) =
     TranslationBundleBuilder(locale).apply(init).build()
